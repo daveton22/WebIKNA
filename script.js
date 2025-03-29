59b2643898cac5071d8801a9b02c365453ba2d9c
daveton22 <devton@students.amikom.ac.id>
2025-03-29 09:31:01 +0700

    new

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // when scroll down, change navbar background color into blur
 window.onscroll = function () {
   var nav = document.querySelector("nav");
-  if (window.scrollY > 100) {
-    nav.classList.add("nav-scroll");
+  var dropdownMenu = document.querySelector(".dropdown-menu");
+
+  if (window.scrollY > 10) {
+    nav.classList.add("scroll");
+    dropdownMenu.classList.add("scroll");
   } else {
-    nav.classList.remove("nav-scroll");
+    nav.classList.remove("scroll");
+    dropdownMenu.classList.remove("scroll");
   }
 };
+
+// when hover kegiatan, rotate the arrow icon
+document.querySelector(".dropdown").addEventListener("mouseover", () => {
+  document.querySelector(".fa-caret-left").style.transform = "rotate(-90deg)";
+});
+
+document.querySelector(".dropdown").addEventListener("mouseout", () => {
+  document.querySelector(".fa-caret-left").style.transform = "rotate(0deg)";
+});
