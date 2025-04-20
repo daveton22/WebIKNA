1f73a5bcb3eb53192bd17f2f5d2ac55d0d03fcfd
daveton22 <devton@students.amikom.ac.id>
2025-04-20 16:39:08 +0700

    new

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ document.querySelector(".dropdown").addEventListener("mouseover", () => {
 document.querySelector(".dropdown").addEventListener("mouseout", () => {
   document.querySelector(".fa-caret-left").style.transform = "rotate(0deg)";
 });
+
+// make me a javascript autoslide image using setInterval
+var counter = 1;
+setInterval(() => {
+  var firstImage = document.querySelector(".first");
+  if (counter === 1) {
+    firstImage.style.marginLeft = "0%";
+  } else if (counter === 2) {
+    firstImage.style.marginLeft = "-20%";
+  } else if (counter === 3) {
+    firstImage.style.marginLeft = "-40%";
+    counter = 0; // Reset counter after the last slide
+  }
+  counter++;
+}, 5000);
